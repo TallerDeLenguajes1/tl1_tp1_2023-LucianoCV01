01d77ed75e245c94af4a42d7c214e8a1a16cb2cd
LucianoCV01 <Lucianocosentino01@gmail.com>
2023-03-21 22:46:48 -0300

    Desarrolle el punto 3.e

--- tp1_1.c
+++ tp1_1.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
 int main(){
-    printf("Hola Mundo");
+    printf("Hola Mundo\n"); //3.a
+    //3.e
+    int numero=6, *puntero=&numero;
+    printf("La dirección de memoria almacenada por el puntero: %p\n", puntero);
+    printf("La dirección de memoria de la variable: %p\n", &numero);
+    printf("La dirección de memoria del puntero: %p\n", &puntero);
+    printf("El tamaño de memoria utilizado por esa variable usando la función sizeof(): %lu\n", sizeof(numero));
+    printf("El contenido del puntero: %p\n", puntero);
+
     return 0;
 }
